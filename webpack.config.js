6c922da3d182fce44f265ebcc82ff86fc5249d71
sarahbooger <sarah.lodge@uconn.edu>
2017-04-06 23:17:17 -0400

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-function buildConfig(env) {
-    return require('./config/webpack.' + env + '.config.js');
-}
-
-module.exports = buildConfig(env === 'production' ? 'prod': 'dev');
-
